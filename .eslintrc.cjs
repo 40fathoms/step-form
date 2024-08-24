d997934e89ebd3c1f334d876414b217dda492adb
40fathoms <jpamdev@gmail.com>
2024-08-24 20:03:59 -0300

    chore: app config

---
+++ .eslintrc.cjs
@@ -0,0 +1,130 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  globals: { React: true, JSX: true },
+  ignorePatterns: [
+    'dist',
+    '.eslintrc.cjs',
+    'tailwind.config.cjs',
+    'postcss.config.cjs'
+  ],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 'latest',
+    ecmaFeatures: { jsx: true }
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  plugins: [
+    'react-refresh',
+    'react',
+    'react-hooks',
+    'prettier',
+    'unicorn',
+    'import'
+  ],
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        semi: true,
+        tabWidth: 2,
+        useTabs: false,
+        singleQuote: true,
+        jsxSingleQuote: false,
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'always',
+        endOfLine: 'auto',
+        trailingComma: 'none',
+        proseWrap: 'always'
+        // tailwindFunctions: ['tv', 'cn'],
+        // tailwindConfig: './tailwind.config.cjs',
+        // plugins: ['prettier-plugin-tailwindcss']
+      }
+    ],
+    '@typescript-eslint/no-unused-vars': ['warn', { varsIgnorePattern: '^_' }],
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        disallowTypeAnnotations: true,
+        fixStyle: 'separate-type-imports',
+        prefer: 'type-imports'
+      }
+    ],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        format: ['PascalCase'],
+        selector: ['typeLike', 'enumMember']
+      },
+      {
+        format: ['PascalCase'],
+        selector: 'interface'
+      }
+    ],
+    '@typescript-eslint/no-explicit-any': 'error',
+    'import/default': 'off',
+    'import/export': 'off',
+    'import/first': 'error',
+    'import/namespace': 'off',
+    'import/newline-after-import': 'error',
+    'import/no-absolute-path': 'error',
+    'import/no-duplicates': 'error',
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-unresolved': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index'
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: false
+        }
+      }
+    ],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-var': 'error',
+    'object-shorthand': 'warn',
+    'prefer-const': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react/button-has-type': 'warn',
+    'react/jsx-pascal-case': 'warn',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'unicorn/prefer-node-protocol': 'warn'
+  },
+  overrides: [
+    {
+      files: ['vite.config.ts'],
+      rules: {
+        'no-unused-vars': 'off',
+        'import/no-default-export': 'off'
+      }
+    }
+  ]
+};
---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+
+function App() {
+  useEffect(() => {
+    const _asad = {
+      1: 34234,
+      2: 45243
+    };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
