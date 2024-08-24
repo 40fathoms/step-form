7bd1e15ac2957bb8b534396e9e6d936738e45ee8
40fathoms <jpamdev@gmail.com>
2024-08-24 20:04:41 -0300

    chore: app config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-import path from "node:path";
+import path from 'node:path';
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3000
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 });
